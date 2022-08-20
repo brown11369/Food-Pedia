37337dda5b86c78286848713d42fa260b5fb0030
Brown <brown11369@gmail.com>
2022-08-20 13:25:02 +0530

    ui Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,12 @@
-import { useState, useEffect } from "react"
+import "./Home.css"
+import { useState, useRef, useEffect } from "react"
 
 const Home = () => {
 
+    let show = useRef();
+
     let [food, setFood] = useState([])
+    let [dish, setDish] = useState({})
 
     useEffect(() => {
         fetch("https://pms-api-food.herokuapp.com/products")
@@ -22,22 +26,38 @@ const Home = () => {
         <>
             <h1 className="container bg-light">food-pedia</h1>
 
-            <div className="d-inline-flex p-2 bd-highlight">
+            <div className="d-inline-flex p-3 bd-highlight gap-2">
                 {
                     food.map((item, ind) => {
                         return (
                             <div key={ind} className="card">
-                                <img src={item.image} className="card-img-top" alt="..." style={{ width: "20%" }} />
+                                <img src={item.image} className="card-img-top img-fluid" alt="..." style={{height:"150px"}} />
                                 <div className="card-body">
                                     <h5 className="card-title">{item.name}</h5>
                                     <p className="card-text">{item.description}</p>
-                                    <a href="#" className="btn btn-primary">View Ingredients</a>
+                                    <button onClick={() => {
+                                        show.current.className = "pop-up"
+                                        setDish(item);
+                                    }} className="btn btn-primary">View More</button>
                                 </div>
                             </div>
                         )
                     })
                 }
 
+
+            </div>
+            <div ref={show} className="hide">
+                <div className="card food-box">
+                    <div className="cross">
+                        <i onClick={() => {
+                            show.current.className = "hide"
+                        }} class="fa-regular fa-circle-xmark"></i>
+                    </div>
+                    <h3 className="card-title">{dish.name}</h3>
+                    <h5 className="card-text">{dish.ingredients}</h5>
+                    <p className="card-text">{dish.directions}</p>
+                </div>
             </div>
 
 
